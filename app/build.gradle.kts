1e6b0d34b7733bd6faba17dcf9136403c2916aa9
Necomer <yolosoul22@outlook.com>
2026-09-02 21:10:51 -0400

    build: wire app to core modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:data"))
+    implementation(project(":core:media"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
