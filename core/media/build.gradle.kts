93f1a8effb0bbefcaa87b5d68ea3f06a1d55af30
Necomer <yolosoul22@outlook.com>
2026-09-02 22:02:58 -0400

    build: add Media3

--- core/media/build.gradle.kts
+++ core/media/build.gradle.kts
@@ -8,6 +8,9 @@ android {
 
 dependencies {
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.session)
+    implementation(libs.androidx.media3.common)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
 }
